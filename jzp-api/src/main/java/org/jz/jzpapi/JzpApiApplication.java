5f8e972c6b5f10357e12f7518359c43ce061b93c
jeremyz1989 <635072863@qq.com>
2018-02-08 16:07:07 +0800

    api client

---
+++ jzp-api/src/main/java/org/jz/jzpapi/JzpApiApplication.java
@@ -0,0 +1,21 @@
+package org.jz.jzpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * api platform
+ *
+ * @author Hongyi Zheng
+ * @date 2018-2-8
+ *
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class JzpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JzpApiApplication.class, args);
+	}
+}
