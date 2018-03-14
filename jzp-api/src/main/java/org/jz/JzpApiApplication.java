d20c02638adb17183665b0c6bf46bac6fdd7f58b
hongyi zheng <635072863@qq.com>
2018-03-14 10:03:15 +0800

    update banner

--- jzp-api/src/main/java/org/jz/JzpApiApplication.java
+++ jzp-api/src/main/java/org/jz/JzpApiApplication.java
@@ -3,10 +3,7 @@ package org.jz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -19,14 +16,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-@ServletComponentScan
 @EnableTransactionManagement
 @Configuration
 @EnableEurekaClient
 public class JzpApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JzpApiApplication.class, args);
+		SpringApplication.run(JzpApiApplication.class,args);
 	}
 
 	
