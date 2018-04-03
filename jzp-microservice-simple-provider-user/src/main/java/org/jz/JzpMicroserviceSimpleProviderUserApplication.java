5accf2353e586c57b4db28609d311935fec4ad6e
hongyi zheng <635072863@qq.com>
2018-04-03 18:01:52 +0800

    整合actuator 端点监控

--- jzp-microservice-simple-provider-user/src/main/java/org/jz/JzpMicroserviceSimpleProviderUserApplication.java
+++ jzp-microservice-simple-provider-user/src/main/java/org/jz/JzpMicroserviceSimpleProviderUserApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * spring cloud 服务提供者
  * spring boot version -- 1.4.3.RELEASE
- * 发现spring boot 2 在controller中会报错 :无法推断参数 ‘S’的类型，该类型应该继承User类，改为1.4.3版本不再报错，
- * 具体原因需要对比源码
+ * TODO 发现spring boot 2 在controller中会报错 :无法推断参数 ‘S’的类型，该类型应该继承User类，改为1.4.3版本不再报错，具体原因需要对比源码
  *
  * @author Hongyi Zheng
  */
