0c58755620efcef278ab7b77dd88fe617271721b
hongyi zheng <635072863@qq.com>
2018-04-03 16:03:50 +0800

    microservice-practice

---
+++ jzp-microservice-simple-provider-user/src/main/java/org/jz/JzpMicroserviceSimpleProviderUserApplication.java
@@ -0,0 +1,12 @@
+package org.jz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzpMicroserviceSimpleProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JzpMicroserviceSimpleProviderUserApplication.class, args);
+	}
+}
