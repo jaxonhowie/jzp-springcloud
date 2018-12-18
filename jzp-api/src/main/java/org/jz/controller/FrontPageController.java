a4b049bc0115ee02d0658bea8186939ec92f730b
hongyi zheng <635072863@qq.com>
2018-12-18 18:49:50 +0800

    update

--- jzp-api/src/main/java/org/jz/controller/FrontPageController.java
+++ jzp-api/src/main/java/org/jz/controller/FrontPageController.java
@@ -1,9 +1,13 @@
 package org.jz.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.jz.service.SteamApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -15,22 +19,21 @@ import java.util.List;
  * @date 2018/2/8
  */
 
-@RestController
+@Controller
 public class FrontPageController {
 
     @Autowired
-    SteamApiController steamApiController;
+    SteamApiService steamApiService;
 
     @RequestMapping("/index")
-    public JSONObject sayHi(){
-        JSONObject jo = new JSONObject();
-        jo.put("I say:","Hi!");
-        return jo;
+    public String sayHi(){
+        return "index";
     }
 
-    @GetMapping("/home")
-    public String homePage(){
-        return "";
+    @RequestMapping("/home")
+    public String homePage(Model model) {
+        model.addAttribute("", steamApiService.queryAllIgnoreDel());
+        return "/api-list";
     }
 
 }
